22c0f54c6219b94c873ccea5ec20de27aa01200d
Prathmesh <prathmeshvairale17@gmail.com>
2024-04-26 13:57:35 +0530

    First commit

--- program1.py
+++ program1.py
@@ -1,11 +1,17 @@
-def smallest_missing_positive_integer(nums: List[int]) -> int:
-    """
-    Implement the function smallest_missing_positive_integer 
-    using the provided smallest_missing_positive_integer function 
-    to find the smallest missing positive integer in the given list.
+def smallest_missing_positive_integer(nums: list[int]) -> int:
+    nums = [num for num in nums if num > 0]
+    
+    n = len(nums)
+    for i in range(n):
+        if abs(nums[i]) - 1 < n and nums[abs(nums[i]) - 1] > 0:
+            nums[abs(nums[i]) - 1] = -nums[abs(nums[i]) - 1]
+    
 
-    """
-    pass
+    for i in range(n):
+        if nums[i] > 0:
+            return i + 1
+    
+    return n + 1
 
 
 
--- program2.py
+++ program2.py
@@ -1,13 +1,15 @@
 def longest_substring(s: str) -> int:
+    if not s:
+        return 0
     
-    """"
-    
-    Implement the function longest_substring 
-    using the provided longest_substring function to find 
-    the length of the longest substring without repeating characters.
+    max_length = 0
+    start = 0
+    char_index_map = {}
 
-    """ 
-    pass
+    for end, char in enumerate(s):
+        if char in char_index_map and char_index_map[char] >= start:
+            start = char_index_map[char] + 1
+        char_index_map[char] = end
+        max_length = max(max_length, end - start + 1)
 
-
-
+    return max_length
